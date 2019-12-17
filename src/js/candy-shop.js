6ba2b94a25a45a96363457684d083e4206cd875c
RonaldoCaetano <ronaldo.caetano@acct.global>
2019-12-16 21:17:06 -0300

    Initial commit

---
+++ src/js/candy-shop.js
@@ -0,0 +1,43 @@
+const data = {
+    title: "Welcome to the Candy Shop",
+    products: [
+        {
+            name: "Zenfone",
+            price: 1234500
+        },
+        {
+            name: "iphone",
+            price: 10000
+        },
+        {
+            name: "Xiaomi",
+            price: "500"
+        }
+    ]
+};
+
+rivets.formatters.price = (val) => {
+    let 
+        spl = String(val).split("."),
+        realArray = spl[0].split(""),
+        lastReal = realArray.length - 1,
+        i;
+    if (realArray.length > 3) {
+        realArray.reverse();
+        for (i = lastReal; i > -1; i--) {
+            if (i % 3 === 0 && i !== 0) realArray.splice(i, 0, ".");
+        }
+        spl[0] = realArray.reverse().join("");
+    }
+    if (spl.length > 1) {
+        spl[1] = spl[1].substr(0, 2);
+        if (spl[1].length < 2) spl[1] += "0";
+    } else {
+        spl[1] = "00";
+    }
+    return 'R$ ' + spl.join(",");
+};
+
+rivets.bind(document.querySelector("#candy-shop"), {
+    data: data
+});
